0bbe509b237c765c0e2edb84b831580d137546e4
Glauco Pater <glauco@monoqi.de>
2019-02-19 13:09:07 +0100

    Fix relative path

--- cli.js
+++ cli.js
@@ -2,13 +2,15 @@
 const json2react = require('./index');
 const fs = require('fs');
 const outputDir = './output';
+var path = require("path");
 
 if(process.argv[2] && fs.existsSync(process.argv[2])){
     if (!fs.existsSync(outputDir)){
         fs.mkdirSync(outputDir);
     }
     const inputFile = process.argv[2];
-    json2react.parse(inputFile);
+    var absolutePath = path.resolve(inputFile);
+    json2react.parse(absolutePath);
 }
 else 
     console.log('No input file selected');
