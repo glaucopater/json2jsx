f7adfc7fbc0875cad1be22f4ff888da0585f0da5
Glauco Pater <>
2019-08-12 22:36:14 +0200

    Small refactor

--- helpers/functions.js
+++ helpers/functions.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = {
   recursive_rendering: function(string, context, stack) {
-    for (var key in context) {
+    for (let key in context) {
       if (context.hasOwnProperty(key)) {
         if (typeof context[key] === "object") {
           string = module.exports.recursive_rendering(
@@ -11,9 +11,9 @@ module.exports = {
             (stack ? stack + "." : "") + key
           );
         } else {
-          var find =
+          let find =
             "\\$\\{\\s*" + (stack ? stack + "." : "") + key + "\\s*\\}";
-          var re = new RegExp(find, "g");
+          let re = new RegExp(find, "g");
           string = string.replace(re, context[key]);
         }
       }
@@ -27,30 +27,30 @@ module.exports = {
   },
   getCurrentDate: function() {
     const dateToken = [];
-    now = new Date();
+    const now = new Date();
     dateToken.push(now.getFullYear());
-    month = now.getMonth() + 1;
+    let month = now.getMonth() + 1;
     if (month.length == 1) {
       month = "0" + month;
     }
     dateToken.push(month);
-    day = now.getDate();
-    if (day.length == 1) {
+    let day = now.getDate();
+    if (day.length === 1) {
       day = "0" + day;
     }
     dateToken.push(day);
-    hour = now.getHours();
-    if (hour.length == 1) {
+    let hour = now.getHours();
+    if (hour.length === 1) {
       hour = "0" + hour;
     }
     dateToken.push(hour);
-    minute = now.getMinutes();
-    if (minute.length == 1) {
+    let minute = now.getMinutes();
+    if (minute.length === 1) {
       minute = "0" + minute;
     }
     dateToken.push(minute);
-    second = now.getSeconds();
-    if (second.length == 1) {
+    let second = now.getSeconds();
+    if (second.length === 1) {
       second = "0" + second;
     }
     dateToken.push(second);
