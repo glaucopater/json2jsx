bc85f6d04dd01aed900ef7b0a3406d96925527ba
Glauco Pater <glauco.pater@gmail.com>
2026-05-31 17:52:52 +0200

    Replace Jest with Vitest for testing framework; remove jest.config.js and update package.json and vitest.config.mjs accordingly. Update dependencies in yarn.lock to reflect changes.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  modulePathIgnorePatterns: [
-    "<rootDir>/output/",
-    "<rootDir>/scripts/playground/",
-  ],
-};
---
+++ vitest.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "node",
+    exclude: [
+      "**/node_modules/**",
+      "**/output/**",
+      "**/scripts/playground/**",
+    ],
+  },
+});
